1bc0928bcf6e91b0790029802721f4ec79d31e87
Kus Andriadi <me@kusandriadi.com>
2019-06-01 01:03:20 +0700

    fix code

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,22 @@
 
 echo -e "\033[0;32mDeploying updates to GitHub...\033[0m"
 
-echo -e "Remove public"
-rm -rf public
+echo -e "\033[0;32mRemove public\033[0m"
+git rm -rf --cached public
 
-# Build the project.
+echo -e "\033[0;32mForce submodule\033[0m"
+git submodule add --force -b master git@github.com:kusandriadi/kusandriadi.github.io.git public
+
+echo -e "\033[0;32mBuild Module\033[0m"
 hugo -t hugo-theme-minos # if using a theme, replace with `hugo -t <YOURTHEME>`
 
 # Go To Public folder
 cd public
+
+
+echo -e "\033[0;32mPull changes\033[0m"
+git pull
+
 # Add changes to git.
 git add .
 
@@ -18,9 +26,12 @@ msg="rebuilding site `date`"
 if [ $# -eq 1 ]
   then msg="$1"
 fi
+
+echo -e "\033[0;32mCommit Changes\033[0m"
 git commit -m "$msg"
 
 # Push source and build repos.
+echo -e "\033[0;32mPush Changes\033[0m"
 git push origin master
 
 # Come Back up to the Project Root
